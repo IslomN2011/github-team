4a2384aec6651acdb1ae4217248214d94c497917
Your Name <your-email@example.com>
2026-08-15 02:58:10 -0700

    islom

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './components/header/header'
+import Footer from './components/footer/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/home/Home'
+import About from './routes/about/About'
+import Services from './routes/Services/Services'
+import Contact from './routes/contact/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
