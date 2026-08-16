1c61035d030d4cc84b7f2912c3c043078227da53
Ismingiz <email@example.com>
2026-08-16 14:33:15 +0500

    Mening oxirgi o'zgarishlarim

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from './components/header/header'
 import Footer from './components/footer/Footer'
 import { Route, Routes } from 'react-router-dom'
--- src/routes/Services/Services.jsx
+++ src/routes/Services/Services.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
 import './services.css'
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
+	return (
+		<main>
+			<div className='imgServices'>
+				<div className='imgText'>
+					<h1>Services</h1>
+					<p>Home/Services</p>
+				</div>
+			</div>
+		</main>
+	)
 }
 
 export default Services
