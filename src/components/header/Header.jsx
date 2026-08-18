c776282efbfd39e4c02ddb2d3fc5180cd634379e
Your Name <your-email@example.com>
2026-08-18 02:03:41 -0700

    baout boldi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    
+      <div className="header-container">
+     
+      </div>
+    
   )
 }
 
