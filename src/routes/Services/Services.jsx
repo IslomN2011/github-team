cff2a20310851f4e3df284f79e88fc23b07a69b6
Ismingiz <email@example.com>
2026-08-18 09:33:01 +0500

    fix: resolve import paths and jsx syntax error

--- src/routes/Services/Services.jsx
+++ src/routes/Services/Services.jsx
@@ -1,16 +1,17 @@
-import BgImg from './componentA/bgImg/BgImg'
-import Join from './componentA/Join/Join'
-import Types from './componentA/Types/Types'
-import Work from './componentA/Work/Work'
-const Services = () => {
-	return (
-		<div>
-			<BgImg/>
-			<Types/>
-			<Work/>
-			<Join/>
-		</div>
-	)
-}
+import BgImg from "./componentA/bgImg/BgImg";
+import Types from "./componentA/Types/Types";
+import Work from "./componentA/Work/Work";
+import Join from "./componentA/Join/Join";
 
-export default Services
+const Services = () => {
+  return (
+    <div>
+      <BgImg />
+      <Types />
+      <Work />
+      <Join />
+    </div>
+  );
+};
+
+export default Services;
